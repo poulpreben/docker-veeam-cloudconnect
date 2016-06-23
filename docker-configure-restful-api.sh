e5996309154c0755534a99ee56d7a9f5d84f75a0
preben <preben.berg@veeam.com>
2016-06-23 18:14:22 +0200

    Added changes to veeam.class.php during provisioning

--- docker-configure-restful-api.sh
+++ docker-configure-restful-api.sh
@@ -9,4 +9,7 @@ fi
 mv /var/www/html/veeam.php /veeam/veeam.php
 sed -e "s/REPLACE_SERVER/$REST_SERVER/" -e "s/REPLACE_PORT/$REST_PORT/" -e "s/REPLACE_USER/$REST_USER/" -e "s/REPLACE_PASS/$REST_PASS/" /veeam/veeam.php > /var/www/html/veeam.php
 
+mv /var/www/html/veeam.class.php /veeam/veeam.class.php
+sed -e "s/REPLACE_BACKUP_SERVER/$BACKUP_SERVER/" -e "s/REPLACE_BACKUP_REPOSITORY/$BACKUP_REPOSITORY/" -e "s/REPLACE_HARDWARE_PLAN/$HARDWARE_PLAN/" /veeam/veeam.class.php > /var/www/html/veeam.class.php
+
 apache2-foreground
